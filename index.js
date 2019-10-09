77bd7182d14701e50c56c631866b590193490ea9
qq382724935 <382724935@qq.com>
2019-10-09 12:11:42 +0800

    ''

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 /*
  * @Date: 2019-09-28 15:12:29
  * @LastEditors: 刘利军
- * @LastEditTime: 2019-10-09 11:43:00
+ * @LastEditTime: 2019-10-09 12:10:59
  */
 const express = require('express');
 const app = express();
 app.set('secret', 'i2u3ifu4843hhg8hg4849gh4g844fh38');
 app.use(require('cors')());
 app.use(express.json());
-app.use('/', express.static(__dirname + '/b2b'));
+app.use('/b2b', express.static(__dirname + '/b2b'));
 app.use('/uploads', express.static(__dirname + '/uploads'));
 require('./plugins/db')(app);
 require('./routes/admin')(app);
