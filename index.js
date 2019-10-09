95e61241216c457dcdabb073043645746f1f3b04
qq382724935 <382724935@qq.com>
2019-10-09 16:43:21 +0800

    a

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 /*
  * @Date: 2019-09-28 15:12:29
  * @LastEditors: 刘利军
- * @LastEditTime: 2019-10-09 13:23:32
+ * @LastEditTime: 2019-10-09 16:40:32
  */
 const express = require('express');
 const app = express();
 app.set('secret', 'i2u3ifu4843hhg8hg4849gh4g844fh38');
 app.use(require('cors')());
 app.use(express.json());
+app.use('/', express.static(__dirname + '/web'));
 app.use('/b2b', express.static(__dirname + '/b2b'));
 app.use('/uploads', express.static(__dirname + '/uploads'));
 require('./plugins/db')(app);
